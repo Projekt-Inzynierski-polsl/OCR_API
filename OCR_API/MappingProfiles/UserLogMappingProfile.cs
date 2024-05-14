42a53c76fbe0dd908ed0ae54b29ddf8d76208c5d
Aven <j.machnik2002@gmail.com>
2024-05-14 13:18:44 +0200

    Added User log mapping profile

---
+++ OCR_API/MappingProfiles/UserLogMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OCR_API.Entities;
+using OCR_API.ModelsDto.UploadedModelDtos;
+using OCR_API.ModelsDto.UserLogDtos;
+
+namespace OCR_API.MappingProfiles
+{
+    public class UserLogMappingProfile : Profile
+    {
+        public UserLogMappingProfile()
+        {
+            CreateMap<UserLogDto, UserLog>();
+            CreateMap<UserLog, UserLogDto>();
+        }
+    }
+}

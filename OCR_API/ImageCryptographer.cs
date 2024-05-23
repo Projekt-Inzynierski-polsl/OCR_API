b9804905da59475a82fc0f4be71e8923cd75f4bb
Aven <j.machnik2002@gmail.com>
2024-05-23 19:34:42 +0200

    Fixed encrypting and decrypting images

--- OCR_API/ImageCryptographer.cs
+++ OCR_API/ImageCryptographer.cs
@@ -7,45 +7,60 @@ namespace OCR_API
 {
     public class ImageCryptographer
     {
-       public async Task<(byte[], byte[])> EncryptImageAsync(Image image)
-       {
+        public async Task<(byte[], byte[])> EncryptImageAsync(Image image)
+        {
             byte[] imageEncryptionKey;
             byte[] hashedImageEncryptionKey;
 
             using (Aes aes = Aes.Create())
             {
                 aes.GenerateKey();
+                aes.GenerateIV();
                 imageEncryptionKey = aes.Key;
-
-                aes.IV = new byte[aes.BlockSize / 8];
 
                 using (MemoryStream memoryStream = new MemoryStream())
                 {
                     image.SaveAsPng(memoryStream);
-
                     memoryStream.Seek(0, SeekOrigin.Begin);
 
                     using (MemoryStream encryptedMemoryStream = new MemoryStream())
                     {
+                        encryptedMemoryStream.Write(aes.IV, 0, aes.IV.Length);
+
                         using (CryptoStream cryptoStream = new CryptoStream(encryptedMemoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
                         {
                             await memoryStream.CopyToAsync(cryptoStream);
                             cryptoStream.FlushFinalBlock();
                         }
-                        return (encryptedMemoryStream.ToArray(), imageEncryptionKey);
+
+                        using (SHA256 sha256 = SHA256.Create())
+                        {
+                            hashedImageEncryptionKey = sha256.ComputeHash(imageEncryptionKey);
+                        }
+
+                        return (encryptedMemoryStream.ToArray(), hashedImageEncryptionKey);
                     }
                 }
             }
-       }
+        }
         public async Task<byte[]> DecryptImageAsync(byte[] encryptedImage, byte[] hashedImageEncryptionKey)
         {
+            if (hashedImageEncryptionKey.Length != 32)
+            {
+                throw new ArgumentException("Invalid encryption key length. Key must be 256 bits (32 bytes) long.");
+            }
+
             byte[] decryptedImage;
+            byte[] imageEncryptionKey;
+
             using (Aes aes = Aes.Create())
             {
-                aes.IV = new byte[aes.BlockSize / 8];
-
                 using (MemoryStream memoryStream = new MemoryStream(encryptedImage))
                 {
+                    byte[] iv = new byte[aes.BlockSize / 8];
+                    memoryStream.Read(iv, 0, iv.Length);
+                    aes.IV = iv;
+
                     using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
                     {
                         using (MemoryStream decryptedStream = new MemoryStream())
@@ -56,11 +71,10 @@ namespace OCR_API
                     }
                 }
 
-                byte[] decryptedImageEncryptionKey;
                 using (SHA256 sha256 = SHA256.Create())
                 {
-                    decryptedImageEncryptionKey = sha256.ComputeHash(aes.Key);
-                    if (!decryptedImageEncryptionKey.SequenceEqual(hashedImageEncryptionKey))
+                    byte[] computedHashedImageEncryptionKey = sha256.ComputeHash(aes.Key);
+                    if (!computedHashedImageEncryptionKey.SequenceEqual(hashedImageEncryptionKey))
                     {
                         throw new Exception("Invalid encryption key.");
                     }
@@ -69,6 +83,7 @@ namespace OCR_API
 
             return decryptedImage;
         }
+
 
         public Image ConvertIFormFileToImage(IFormFile formFile)
         {

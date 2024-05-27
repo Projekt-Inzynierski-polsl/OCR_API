ff9509cbf838125faf78e660d3c2721777235c83
Aven <j.machnik2002@gmail.com>
2024-05-27 19:59:32 +0200

    Nullable fields in updateUserDto

--- OCR_API/ModelsDto/UserDto/UpdateUserDto.cs
+++ OCR_API/ModelsDto/UserDto/UpdateUserDto.cs
@@ -2,9 +2,9 @@
 {
     public class UpdateUserDto
     {
-        public string Email { get; set; } = null;
-        public string Nickname { get; set; } = null;
-        public string Password { get; set; } = null;
+        public string? Email { get; set; } = null;
+        public string? Nickname { get; set; } = null;
+        public string? Password { get; set; } = null;
         public int? RoleId { get; set; } = null;
 
     }
